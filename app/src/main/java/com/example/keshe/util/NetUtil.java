532aa06ef075fe52b7de13d976468a7a5efde832
Tommy Lin <1065770912@qq.com>
2022-12-11 22:46:40 +0800

    更新天气api

--- app/src/main/java/com/example/keshe/util/NetUtil.java
+++ app/src/main/java/com/example/keshe/util/NetUtil.java
@@ -11,7 +11,7 @@ import java.net.URL;
 
 public class NetUtil {
 
-    public static final String URL_WEATHER_WITH_FUTURE = "https://tianqiapi.com/api?version=v1&appid=36646344&appsecret=c1lgQbP9";
+    public static final String URL_WEATHER_WITH_FUTURE = "https://yiketianqi.com/free/day?appid=48453555&appsecret=bPCgbZl2&";
 
 
     public static String doGet(String urlStr) {
